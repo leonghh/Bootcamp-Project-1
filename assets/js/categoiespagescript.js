c658dacb9c80f2a4c0f8c2d8785dd80a3383e9dc
jsstringer <jsamuelstringer@gmail.com>
2019-12-20 14:08:36 +1100

    categorychanges

--- assets/js/categoiespagescript.js
+++ assets/js/categoiespagescript.js
@@ -1,28 +1,62 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+  //$.ajax({ url: "categoiespageindex.html",
+  //context: document.body,
+  //success: function(){
+   //  alert("done");
+  //}});
 
-    $(".Games-PuzzlesCards").click(function() {
-        window.location.href='productspage.html';
-        console.log("Games-puzzleclick");
-    });
+    //$("")
 
-    var recommendations = $(this).attr("data-recommendations");
+
+    //$(".Games-PuzzlesCards").click(function() {
+    //    window.location.href='santaitem.html';
+    //    console.log("Games-puzzleclick");
+
+
+    var customerTopRated = $(this).attr("data-customerTopRated");
+    
+
 
     var queryURL = "https://api.bestbuy.com/v1/products((search=toys)&)?apiKey=Zi5gCWq3TCGhqs5xR5aK2hU3&sort=thumbnailImage.asc&show=description,image,thumbnailImage&facet=bestSellingRank&format=json";
 
-    $.ajax({
+
+    
+      $.ajax({
         url: queryURL,
-        method: "GET"
-      })
+        method: "GET",
+        context: document.body,
+        success: function(){
+           alert("done");
+        }
 
-        var bby = require('bestbuy')('Zi5gCWq3TCGhqs5xR5aK2hU3');
-        bby.categories('',{show:'id'}).then(function(data){
-        console.log(data);
-        });
+        .then(function(response) {
+        console.log(queryURL);
+
+        console.log(response);
+
+        var results = response.customerTopRated;
+        console.log(results);
+
+        for (var i = 0; i < results.length; i++)
+
+        var productDiv = $("<div>");
+        var productName = $("<h3>");
+        var productCaption = $("<p>");
+        var productImage = $("<img>");
+
         
-        var bby = require('bestbuy')('Zi5gCWq3TCGhqs5xR5aK2hU3');
-        bby.recommendations('trendingViewed','abcat0400000').then(function(data){
-        console.log(data);
-        });
+        productImage.attr("src", results[i].image);
 
-});
+            productName.text(results[i].name);
+            productCaption.text(results[i].Description);
+
+            console.log(results);
+
+            $("#siteloader).append(productDiv);
+            productDiv.append(productImage);
+            productDiv.append(productName);
+            productDiv.append(productCaption);
+      })
+    })  
+
+  });
